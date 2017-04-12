e3f68fe9d75fd4e19a7f91934a6a191bcb25512c
Valentin Malykh <madrugado@yandex.ru>
2017-04-12 20:50:17 +0300

    TEMP: evening

--- src/deep_dialog/nlu/data_generator.py
+++ src/deep_dialog/nlu/data_generator.py
@@ -1,25 +1,40 @@
 import json
+import random
 
 
 def template_to_BIO(template):
     markup = []
-    for token in template.split():
+    for token in template:
         if token[0] == token[-1] == "$":
-            pass
+            markup.append("B-" + token.split("_")[0][1:])
+        else:
+            markup.append("O")
+    return markup
 
 
 class NLUDataGenerator:
-    def __init__(self, path_to_template, path_to_dict):
-        with open(path_to_dict, "wt") as f_dict:
+    def __init__(self, path_to_template, path_to_dict, batch_size=32):
+        self.batch_size = batch_size
+        with open(path_to_dict, "rt") as f_dict:
             self.dict = json.load(f_dict)
-        with open(path_to_template, "wt") as f_tmpl:
+        with open(path_to_template, "rt") as f_tmpl:
             templates = json.load(f_tmpl)
 
         self.templates = []
         for t in templates:
-            self.templates.append((t["nl"]["agt"], "agt"))
-            self.templates.append((t["nl"]["usr"], "usr"))
+            agent_line = t["nl"]["agt"].split()
+            self.templates.append((agent_line, template_to_BIO(agent_line), "agt"))
+
+            user_line = t["nl"]["usr"].split()
+            self.templates.append((user_line, template_to_BIO(user_line), "usr"))
 
     def __next__(self):
-        pass
-
+        batch = random.sample(self.templates, self.batch_size)
+        filled_batch = []
+        for t, m, r in batch:
+            input_ = []
+            target = []
+            for i in range(len(m)):
+                if m[i] == 'O':
+                    input_.append(t[i])
+                    target.append(m[i])
